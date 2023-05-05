8b70fd817319fa243c36237bbbe2a27ac396fea1
MORENO CÉSAR SANTOS SANTIAGO <22283401@cfp127aluno.local>
2023-05-05 11:29:59 -0300

    5/5/23

--- app.js
+++ app.js
@@ -31,16 +31,18 @@ app.use((request, response, next) => {
 // CRUD (Create, Read, Update e Delete)
 
 /*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*
-* EndPoint: Tabela de alunos
-* Data: 14/04/2023
-* Versão: 1.0
-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*/
+ * EndPoint: Tabela de alunos
+ * Data: 14/04/2023
+ * Versão: 1.0
+ *-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*/
 
 const bodyJSON = bodyParser.json();
 // Import da controller do aluno
 let controllerAluno = require('./controller/controller_aluno.js');
 // EndPoint: Retorna todos os dados do aluno
-app.get('/v1/lion-school/aluno', cors(), async function (request, response) {
+app.get('/v1/lion-school/aluno', cors(), async function(request, response) {
+
+
 
     // Solicita a controller que retorna todos os alunos do BD
     let dados = await controllerAluno.selecionarTodosAlunos();
@@ -57,12 +59,15 @@ app.get('/v1/lion-school/aluno', cors(), async function (request, response) {
 });
 
 // EndPoint: Retorna todos os dados do aluno pelo ID
-app.get('/v1/lion-school/:id', cors(), async function (request, response) {
-});
+app.get('/v1/lion-school/:id', cors(), async function(request, response) {});
 // EndPoint: Inserir um novo aluno
-app.post('/v1/lion-school/aluno', cors(), bodyJSON, async function (request, response) {
+app.post('/v1/lion-school/aluno', cors(), bodyJSON, async function(request, response) {
+    
+    
+    
     // Recebe os dados encaminhados no body da requisição
     let dadosBody = request.body;
+
     console.log(dadosBody);
 
 
@@ -70,7 +75,7 @@ app.post('/v1/lion-school/aluno', cors(), bodyJSON, async function (request, res
     let resultInsertDados = await controller_aluno.inserirAluno(dadosBody);
 
     console.log(resultInsertDados);
-    
+
 
     // Retorna o status code e a message
     response.status(resultInsertDados.status);
@@ -78,12 +83,21 @@ app.post('/v1/lion-school/aluno', cors(), bodyJSON, async function (request, res
 
 });
 // EndPoint: Atualiza um aluno pelo ID
-app.put('/v1/lion-school/:id', cors(), async function (request, response) {
+app.put('/v1/lion-school/:id', cors(), bodyJSON, async function(request, response) {
+    // Recebe os dados do body
+    let dadosBody = request.body;
+
+    // Recebe o id do aluno
+    let idAluno = request.params.id;
+
+    let resultUpdateDados = await controllerAluno.atualizarAluno(dadosBody, idAluno);
+
+    response.status(resultUpdateDados.status);
+    response.json(resultUpdateDados);
 });
 // EndPoint: Exclui um aluno pelo ID
-app.delete('/v1/lion-school/:id', cors(), async function (request, response) {
-});
+app.delete('/v1/lion-school/:id', cors(), async function(request, response) {});
 
-app.listen(8080, function () {
+app.listen(8080, function() {
     console.log('Servidor aguardando requisição');
 });
